0110f85ef4aa2c86b1a79d7c831870b0978c18bc
xXKlaskpffXx <leander.jonte.ulrich@gmail.com>
2024-01-17 18:39:15 +0100

    save

---
+++ utilitj/functions.py
@@ -0,0 +1,11 @@
+def avg(list: [int]) -> float:
+    y = 0
+    for x in int:
+        y += x
+    return y/len(list)
+
+def sum(list: [int]) -> int:
+    y = 0
+    for x in list:
+        y += x
+    return y
